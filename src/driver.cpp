ee60d45ffd8c9880dff87a9b3aa39fabb8b65743
Zhven <sepaap@gmail.com>
2023-05-04 00:29:13 +0300

    first attempt at plugins

--- src/driver.cpp
+++ src/driver.cpp
@@ -1,5 +1,6 @@
 #include "robotont_driver/driver.hpp"
 #include "robotont_driver/hardware.hpp"
+#include "robotont_driver/odom.hpp"
 
 
 //namespace drivers
@@ -17,6 +18,8 @@ namespace robotont
     // Create a shared pointer of this node to allow ros functionality in subclasses
     auto node_ptr = shared_from_this();
     hw_ptr_ = std::make_shared<Hardware>(node_ptr);
+
+    pluginlib::ClassLoader<robotont::Odom> Odom("PluginBase", "robotont::Odom");
 
     
     timer_ = this->create_wall_timer(
